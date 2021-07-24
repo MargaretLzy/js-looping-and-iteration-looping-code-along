51f5ab12f669d34bba083835c58f912c704b2506
MargaretLzy <43190680+MargaretLzy@users.noreply.github.com>
2021-07-23 22:45:36 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    let result =[]
+    for (let i = 0; i < array.length; i++) {
+       result.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+      }
+      return result;
+    
+}
+function countDown(int)
+{
+    let count =0
+    while(count<=int)
+    {
+        console.log(int--);
+    }
+}
